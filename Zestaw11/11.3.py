5d02a0ddd7c085bdbe17f215ca75f45ac086bd53
Karol Wójcik <44023217+incergg@users.noreply.github.com>
2019-01-30 19:43:14 +0100

    Add files via upload

--- Zestaw11/11.3.py
+++ Zestaw11/11.3.py
@@ -1,21 +1,31 @@
 def cmp(a, b):
-    if a > b:
-        return 1
-    elif a < b:
-        return -1
-    return 0
+	if a > b:
+		return 1
+	elif a < b:
+		return -1
+	return 0
+	
+	
+def insertsort(L, left=0, right=0, cmpfunc=cmp):
+	if right == 0:
+		right = len(L)-1
+	for i in range(right, left, -1): 
+		if cmpfunc(L[i-1],L[i]) > 0: 
+			L[i-1],L[i] = L[i],L[i-1]
+	for i in range(left+2, right+1):
+		j = i
+		item = L[i]
+		while cmpfunc(item, L[j-1]) < 0:
+			L[j] = L[j-1]
+			j = j-1
+		L[j] = item
+	
+	
+L1 = [6,1,3,3,2,5,1,7,0]
+L2 = [4,2,3,6,1,2,8,5,9]
+
+insertsort(L1)
+insertsort (L2,0,6)
 
 
-def insertsort(L, left, right, cmpfunc=cmp):
-    for i in range(right, left, -1): 
-        if cmpfunc(L[i-1],L[i]): 
-            L[i-1],L[i] = L[i],L[i-1]
-    for i in range(left+2, right+1):
-        j = i
-        item = L[i]
-        while item < L[j-1]:
-            L[j] = L[j-1]
-            j = j-1
-        L[j] = item
-		
-		
+print(L1,"\n",L2)
