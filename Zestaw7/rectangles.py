7ef064bc581fbdbc52d4128cc6d76a56b058a5bb
Karol Wójcik <44023217+incergg@users.noreply.github.com>
2019-01-30 18:45:40 +0100

    Add files via upload

--- Zestaw7/rectangles.py
+++ Zestaw7/rectangles.py
@@ -1,36 +1,66 @@
 from points import Point
 
 class Rectangle:
-    """Klasa reprezentująca prostokąt na płaszczyźnie."""
-
-    def __init__(self, x1, y1, x2, y2):
-        if x1>x2 or y1>y2:
-            raise ValueError("Punkty powinny wskazywać lewny dolny i prawy górny róg!!!") 
-        self.pt1 = Point(x1, y1)
-        self.pt2 = Point(x2, y2)
-
-    def __str__(self):         # "[(x1, y1), (x2, y2)]"
-    	return("[("+str(self.pt1.x)+", "+str(self.pt1.y)+"), ("+str(self.pt2.x)+", "+str(self.pt2.y)+")]")
-    	
-    def __repr__(self):        # "Rectangle(x1, y1, x2, y2)"
-    	return("Rectangle("+str(self.pt1.x)+", "+str(self.pt1.y)+", "+str(self.pt2.x)+", "+str(self.pt2.y)+")")
-    
-    def __eq__(self, other):   # obsługa rect1 == rect2
-    	if(self.pt1.x==other.pt1.x):
-    		if(self.pt1.y==other.pt1.y):
-    			if(self.pt2.x==other.pt2.x):
-    				if(self.pt2.y==other.pt2.y):
-    					return True
-    	return False
-    	
-    def __ne__(self, other):      # obsługa rect1 != rect2
-        return not self == other
-
-    def center(self):        # zwraca środek prostokąta
-    	return Point(((self.pt2.x+self.pt1.x)/2 ),((self.pt2.y+self.pt1.y)/2))
+	"""Klasa reprezentująca prostokąt na płaszczyźnie."""
+	
+	def __init__(self, x1, y1, x2, y2):
+		if x1>x2 or y1>y2:
+			raise ValueError("Punkty powinny wskazywać lewny dolny i prawy górny róg!!!") 
+		self.pt1 = Point(x1, y1)
+		self.pt2 = Point(x2, y2)
+	
+	def __str__(self):         # "[(x1, y1), (x2, y2)]"
+		return("[("+str(self.pt1.x)+", "+str(self.pt1.y)+"), ("+str(self.pt2.x)+", "+str(self.pt2.y)+")]")
 		
-    def area(self):             # pole powierzchni
-    	return ((self.pt2.x-self.pt1.x)*(self.pt2.y-self.pt1.y))
+	def __repr__(self):        # "Rectangle(x1, y1, x2, y2)"
+		return("Rectangle("+str(self.pt1.x)+", "+str(self.pt1.y)+", "+str(self.pt2.x)+", "+str(self.pt2.y)+")")
+	
+	def __eq__(self, other):   # obsługa rect1 == rect2
+		if(self.pt1.x==other.pt1.x):
+			if(self.pt1.y==other.pt1.y):
+				if(self.pt2.x==other.pt2.x):
+					if(self.pt2.y==other.pt2.y):
+						return True
+		return False
 		
-    def move(self, x, y):       # przesunięcie o (x, y)
-    	return Rectangle(self.pt1.x+x,self.pt1.y+y,self.pt2.x+x,self.pt2.y+y)
+	def __ne__(self, other):      # obsługa rect1 != rect2
+		return not self == other
+	
+	def center(self):        # zwraca środek prostokąta
+		return Point(((self.pt2.x+self.pt1.x)/2 ),((self.pt2.y+self.pt1.y)/2))
+		
+	def area(self):             # pole powierzchni
+		return ((self.pt2.x-self.pt1.x)*(self.pt2.y-self.pt1.y))
+		
+	def move(self, x, y):       # przesunięcie o (x, y)
+		return Rectangle(self.pt1.x+x,self.pt1.y+y,self.pt2.x+x,self.pt2.y+y)
+
+	def intersection(self, other):
+		if self.pt1.x < other.pt2.x:
+			if self.pt2.x > other.pt1.x:
+				if self.pt2.y > other.pt1.y:
+					if self.pt1.y < other.pt2.y:
+						p1x = max(self.pt1.x, other.pt1.x)
+						p1y = max(self.pt1.y, other.pt1.y)
+						p2x = min(self.pt2.x, other.pt2.x)
+						p2y = min(self.pt2.y, other.pt2.y)
+						return Rectangle(p1x,p1y,p2x,p2y)		
+		return Rectangle(0,0,0,0)
+		
+	def cover(self, other):
+		p1x = min(self.pt1.x, other.pt1.x)
+		p1y = min(self.pt1.y, other.pt1.y)
+		p2x = max(self.pt2.x, other.pt2.x)
+		p2y = max(self.pt2.y, other.pt2.y)
+		return Rectangle(p1x,p1y,p2x,p2y)
+		
+	def make4(self):
+		r = []
+		sx = (self.pt1.x+self.pt2.x)/2
+		sy = (self.pt1.y+self.pt2.y)/2
+		r.append(Rectangle(self.pt1.x,self.pt1.y,sx,sy))
+		r.append(Rectangle(self.pt1.x,sy,sx,self.pt2.y))
+		r.append(Rectangle(sx,sy,self.pt2.x,self.pt2.y))
+		r.append(Rectangle(sx,self.pt1.y,self.pt2.x,sy))
+		return r
+	
--- Zestaw7/test_rectangles.py
+++ Zestaw7/test_rectangles.py
@@ -45,6 +45,32 @@ class TestRectangle(unittest.TestCase):
 	def test_move(self):
 		test = rectangles.Rectangle(1,1,3,2)
 		self.assertEqual(test.move(-1,2),rectangles.Rectangle(0, 3, 2, 4))
-
+		
+	def test_intersection(self):
+		
+		test1 = rectangles.Rectangle(1,1,3,2)
+		test2 = rectangles.Rectangle(0,0,2,2)
+		test3 = rectangles.Rectangle(3,1,4,2)
+		test4 = rectangles.Rectangle(0,1.5,2,3)
+		self.assertEqual(rectangles.Rectangle.intersection(test1,test2), rectangles.Rectangle(1,1,2,2))
+		self.assertEqual(rectangles.Rectangle.intersection(test1,test3), rectangles.Rectangle(0,0,0,0))
+		self.assertEqual(rectangles.Rectangle.intersection(test1,test4), rectangles.Rectangle(1,1.5,2,2))
+		
+	def test_cover(self):
+		test1 = rectangles.Rectangle(1,1,3,2)
+		test2 = rectangles.Rectangle(0,0,2,2)
+		test3 = rectangles.Rectangle(3,1,4,2)
+		test4 = rectangles.Rectangle(0,1.5,2,3)
+		self.assertEqual(rectangles.Rectangle.cover(test1,test2), rectangles.Rectangle(0,0,3,2))
+		self.assertEqual(rectangles.Rectangle.cover(test1,test3), rectangles.Rectangle(1,1,4,2))
+		self.assertEqual(rectangles.Rectangle.cover(test1,test4), rectangles.Rectangle(0,1,3,3))
+		
+	def test_make4(self):
+		test2 = rectangles.Rectangle(0,0,2,2)
+		self.assertEqual(rectangles.Rectangle.make4(test2)[0], rectangles.Rectangle(0,0,1,1) )
+		self.assertEqual(rectangles.Rectangle.make4(test2)[1], rectangles.Rectangle(0,1,1,2) )
+		self.assertEqual(rectangles.Rectangle.make4(test2)[2], rectangles.Rectangle(1,1,2,2) )
+		self.assertEqual(rectangles.Rectangle.make4(test2)[3], rectangles.Rectangle(1,0,2,2) )
+		
 if __name__ == '__main__':
 	unittest.main()
